ab3d4ac80d66102ae043332e593fce0daa1706e8
Viktor <268172490+Peaseblossomm@users.noreply.github.com>
2026-07-14 00:43:57 +0200

    Initial project setup

---
+++ ExamEngine/Entities/Question.cs
@@ -0,0 +1,43 @@
+﻿namespace ExamFactory.Entities
+{
+    public class Question
+    {
+        public int Id { get; private set; }
+
+        public string Context { get; private set; }
+
+        public List<string> Answers { get; private set; }
+
+        public int CorrectAnswerIndex { get; private set; }
+
+        public int Value { get; private set; }
+
+        public Question(
+            string context,
+            string answer1,
+            string answer2,
+            string answer3,
+            string answer4,
+            int correctAnswerIndex,
+            int value)
+        {
+            Context = context;
+
+            Answers = new List<string>
+        {
+            answer1,
+            answer2,
+            answer3,
+            answer4
+        };
+
+            CorrectAnswerIndex = correctAnswerIndex;
+            Value = value;
+        }
+
+        public bool IsCorrect(int selectedAnswerIndex)
+        {
+            return selectedAnswerIndex == CorrectAnswerIndex;
+        }
+    }
+}
---
+++ ExamEngine/Entities/QuestionBank.cs
@@ -0,0 +1,14 @@
+﻿namespace ExamFactory.Entities
+{
+    public class QuestionBank
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+    }
+}
